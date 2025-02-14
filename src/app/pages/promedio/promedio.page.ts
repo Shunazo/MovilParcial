e21de659335e7e6d889cd105003cff2b9c7f9ade
Shunazo <abeljunior0907@gmail.com>
2025-02-14 18:54:59 -0400

    Inicio y Promedio funcionales

--- src/app/pages/promedio/promedio.page.ts
+++ src/app/pages/promedio/promedio.page.ts
@@ -12,5 +12,28 @@ import { IonicModule } from '@ionic/angular'
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonicModule, IonCard, IonButton, IonInput]
 })
 export class PromedioPage  {
-  
+  num1: string= "";
+  num2: string= "";
+  num3: string= "";
+
+  max: number | string = "";
+  min: number | string = "";
+  avg: number | string = "";
+
+  calcularResultados() {
+
+    const n1 = Number(this.num1);
+    const n2 = Number(this.num2);
+    const n3 = Number(this.num3);
+
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)) {
+      this.max = this.min = this.avg = "Ingresa un numero valido manito"
+      return;
+    }
+
+  const nums = [n1, n2, n3]; 
+  this.max = Math.max(...nums);
+  this.min = Math.min(...nums);
+  this.avg = (n1 + n2 + n3) / 3;
+  }
 }
