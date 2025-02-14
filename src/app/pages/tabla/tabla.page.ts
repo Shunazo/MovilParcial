4ee94921df0a927c3aece5247816f23dc0491798
Shunazo <abeljunior0907@gmail.com>
2025-02-14 19:07:11 -0400

    finished

--- src/app/pages/tabla/tabla.page.ts
+++ src/app/pages/tabla/tabla.page.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonButton, IonInput} from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular'
 
 @Component({
   selector: 'app-tabla',
   templateUrl: './tabla.page.html',
   styleUrls: ['./tabla.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonButton, IonInput, IonicModule]
 })
-export class TablaPage implements OnInit {
 
-  constructor() { }
+export class TablaPage {
 
-  ngOnInit() {
+  numero: string = "";
+  resultado: string[] = [];
+
+  generarTabla(n: number) {
+    if (isNaN(n) || n <= 0) return ["Ingrese un numero valido"]
+
+    let table: string[] = [];
+    for (let i = 1; i <= 14; i++) {
+      table.push(`${n} x ${i} = ${n * i}`);
+    }
+    return table;
+  }
+
+  calcularTabla() {
+    const num = Number(this.numero);
+    this.resultado = isNaN(num) ? ["Ingrese un numero valido"]: this.generarTabla(num);
   }
 
 }
